41e3703b3a540ecb8d23db88014a36027c565d19
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-11-28 20:35:50 +0400

    feat: Add contactPageImage function in contactUs.js

---
+++ landingPage/src/scripts/contactUs.js
@@ -0,0 +1,7 @@
+import contactImage from "../image/fish3.png";
+
+function contactPageImage(){
+    const contactPageImage = document.createElement("img")
+    contactPageImage.setAttribute("src", contactImage);
+    return contactPageImage;
+}
