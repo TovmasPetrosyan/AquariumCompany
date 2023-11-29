38bb759389960b39bc9832945c73909afcbfcd86
MeriMkrtchyann <142087396+MeriMkrtchyann@users.noreply.github.com>
2023-11-29 17:31:15 +0400

    Revert "Main page"

--- landingPage/src/scripts/createAquarium.js
+++
@@ -1,9 +0,0 @@
-import img from "../image/aquarium.jpg"
-
-function aquariumImage(){
-    const aquariumImg = document.createElement("img")
-    aquariumImg.setAttribute("src", img)
-    return aquariumImg
-}
-
-export default aquariumImage
--- landingPage/src/scripts/createNamCompany.js
+++
@@ -1,7 +0,0 @@
-function companyName (){
-    const companyName = document.createElement("h1")
-    companyName.textContent = "FishHome";
-    return companyName
-}
-
-export default companyName
