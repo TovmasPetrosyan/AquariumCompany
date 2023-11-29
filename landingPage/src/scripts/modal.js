937f42d8e0101aafb45381674d1a150c7b1e0b85
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-11-30 00:50:40 +0400

    fear: add modal.js file

---
+++ landingPage/src/scripts/modal.js
@@ -0,0 +1,29 @@
+import modalImg from '../image/contact-thank-you.svg'
+
+function createModal(){
+    const modalWrap = document.createElement("div");
+    modalWrap.setAttribute("class", "modal-wrap display-none");
+   
+    const modal = document.createElement("div");
+          modal.setAttribute("class", "modal");
+          modalWrap.appendChild(modal);
+    const img = document.createElement("img"); 
+          img.setAttribute("src",modalImg);
+    const par1 = document.createElement("p");
+          par1.innerText = "Thank you for your submission!";
+    const par2 = document.createElement("p");
+          par2.innerText = "We will be back in touch in 24-48 hours.";
+    const button = document.createElement("button");
+          button.setAttribute("class", "btn modal-close");
+          button.textContent = "Close";
+     
+          
+          modal.appendChild(img);
+          modal.appendChild(par1);
+          modal.appendChild(par2);
+          modal.appendChild(button);
+
+    return modalWrap
+}
+
+export default createModal
