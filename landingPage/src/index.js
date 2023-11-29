bd28e5376f5a71ded44890c260cd933340523c3d
Meri Mkrtchyan <meri.mkrtchyan1999@gmail.com>
2023-11-29 16:01:44 +0400

    add eventListener for scrol page

--- landingPage/src/index.js
+++ landingPage/src/index.js
@@ -5,15 +5,28 @@ import createHomePage from "./scripts/createHomePage"
 import createNav from "./scripts/navScript"
 import createMain from "./scripts/mainScript"
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root")
-    const nav = createNav()
-    root.appendChild(nav)
-    root.appendChild(createHomePage())
-    const header = createMain()
-    root.appendChild(header)
-})
-
+    const root = document.getElementById("root");
+    const nav = createNav();
+    root.appendChild(nav);
+    root.appendChild(createHomePage());
+    const header = createMain();
+    root.appendChild(header);
+  
+    const navLinks = document.querySelectorAll('.nav ul li a');
+    navLinks.forEach(function (navLink) {
+      navLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        let targetId = event.target.getAttribute('href').substring(1);
+        let targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+              behavior: 'smooth'
+            });
+          }
+      });
+    });
+  });
+  
 
 
