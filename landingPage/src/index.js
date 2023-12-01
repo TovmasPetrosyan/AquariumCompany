5ff38797e042e7f1faab649be401dc5cddb4b772
suren <tadevosyansuren07@gmail.com>
2023-12-01 13:08:13 +0400

    created aquarium section

--- landingPage/src/index.js
+++ landingPage/src/index.js
@@ -1,32 +1,30 @@
-import "./styles/navStyle.css"
-import "./styles/mainStyle.css"
-import "./styles/headerStyle.css"
-import createHomePage from "./scripts/createHomePage"
-import createNav from "./scripts/navScript"
-import createMain from "./scripts/mainScript"
+import "./styles/navStyle.css";
+import "./styles/mainStyle.css";
+import "./styles/headerStyle.css";
+import "./styles/aquaStyle.css";
+import createHomePage from "./scripts/createHomePage";
+import createNav from "./scripts/navScript";
+import createMain from "./scripts/mainScript";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
-    const nav = createNav();
-    root.appendChild(nav);
-    root.appendChild(createHomePage());
-    const header = createMain();
-    root.appendChild(header);
-  
-    const navLinks = document.querySelectorAll('.nav ul li a');
-    navLinks.forEach(function (navLink) {
-      navLink.addEventListener('click', function (event) {
-        event.preventDefault();
-        let targetId = event.target.getAttribute('href').substring(1);
-        let targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            targetElement.scrollIntoView({
-              behavior: 'smooth'
-            });
-          }
-      });
+  const root = document.getElementById("root");
+  const nav = createNav();
+  root.appendChild(nav);
+  root.appendChild(createHomePage());
+  const header = createMain();
+  root.appendChild(header);
+
+  const navLinks = document.querySelectorAll(".nav ul li a");
+  navLinks.forEach(function (navLink) {
+    navLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      let targetId = event.target.getAttribute("href").substring(1);
+      let targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
     });
   });
-  
-
-
+});
